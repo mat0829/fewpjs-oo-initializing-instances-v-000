dc50156ab1e48e294066ce0d0eb794b0d093250a
Mat Anderson <matandersonlv@hotmail.com>
2020-02-17 22:25:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here
 
-class Dog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink
   }
 }
